47a27b72752d80530a1fd49ae55abbb4b9061130
Fabian Lessmann <lessmannwebdev@gmail.com>
2025-02-14 21:47:09 +0100

    stop banner video when not in viewport

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { ArrowRightCircle } from "lucide-react";
 import Video from "next-video";
-import { PropsWithChildren } from "react";
+import { useRef } from "react";
+import PaddingBox from "./layout/PaddingBox";
 import { Button } from "./ui/button";
 import previewVideo from "/videos/Castle Appears in autnumnal and foggy forest.mp4";
-import PaddingBox from "./layout/PaddingBox";
+import { motion } from "framer-motion";
 
-function Banner({ children }: PropsWithChildren<{}>) {
+function Banner() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
   const handleButtonClick = (destination: string) => {
     // scroll to section #contact
     const contactSection = document.getElementById(destination);
@@ -15,8 +18,31 @@ function Banner({ children }: PropsWithChildren<{}>) {
 
   return (
     <PaddingBox bottom={"md"}>
-      <div className="w-full h-[calc(100vh*0.36)] overflow-hidden mt-[10rem] flex items-center justify-center relative">
-        <Video src={previewVideo} controls={false} muted={true} autoPlay loop />
+      <motion.div
+        className="w-full h-[calc(100vh*0.36)] overflow-hidden mt-[10rem] flex items-center justify-center relative"
+        onViewportEnter={() => {
+          if (videoRef.current) {
+            videoRef.current.muted = true; // Muted für Autoplay aktivieren
+            videoRef.current
+              .play()
+              .catch((err) => console.warn("Autoplay blockiert:", err));
+          }
+        }}
+        onViewportLeave={() => {
+          if (videoRef.current) {
+            videoRef.current.pause();
+            videoRef.current.currentTime = 0;
+          }
+        }}
+      >
+        <Video
+          src={previewVideo}
+          controls={false}
+          muted
+          autoPlay
+          loop
+          ref={videoRef}
+        />
         <div className="absolute inset-0 flex flex-col justify-center items-center ">
           <h2 className="text-white font-medium mb-4">Eine neue Perspektive</h2>
           <Button
@@ -29,7 +55,7 @@ function Banner({ children }: PropsWithChildren<{}>) {
             Jetzt kontaktieren
           </Button>
         </div>
-      </div>
+      </motion.div>
     </PaddingBox>
   );
 }
