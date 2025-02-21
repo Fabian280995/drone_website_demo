cd7116c8d086fbe55fca0a290622c339e0ba976f
Fabian Lessmann <lessmannwebdev@gmail.com>
2025-02-21 23:50:23 +0100

    Logo ist nun Link

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,16 +1,17 @@
-import { motion } from "framer-motion";
+import Link from "next/link";
 
 const Logo = () => {
   return (
-    <motion.h1
+    <Link
+      href="/"
       className="uppercase text-gray-900 flex flex-col font-medium"
       aria-label="Cinematic Eagle FPV - Logo"
     >
-      <span className="tracking-[0.09em] leading-8">Fabian Lessmann</span>
-      <span className="text-lg md:text-xl xl:text-3xl tracking-[0.1em] text-gray-600">
+      <h1 className="tracking-[0.09em] leading-8">Fabian Lessmann</h1>
+      <h2 className="text-lg md:text-xl xl:text-3xl tracking-[0.1em] text-gray-600">
         CineEagle
-      </span>
-    </motion.h1>
+      </h2>
+    </Link>
   );
 };
 
