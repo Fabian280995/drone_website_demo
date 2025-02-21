00dbda976bed00fbbf69f886b62f4649d92ed7b6
Fabian Lessmann <lessmannwebdev@gmail.com>
2025-02-21 23:45:01 +0100

    Änderungen für deployment auf vercel

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -2,13 +2,15 @@ import { NavLinks as NavLinksData } from "@/data/navLinks";
 import { cn } from "@/lib/utils";
 
 const NavLinks = ({ onClick }: { onClick?: () => void }) => {
+  const handleOnClick = () => {
+    if (onClick) onClick();
+  };
+
   return NavLinksData.map((link) => (
     <li key={link.title}>
       <a
         href={link.href}
-        onClick={() => {
-          onClick && onClick();
-        }} // Scroll-Funktion nur für "Home"
+        onClick={handleOnClick}
         className={cn(
           "uppercase text-xl transition-colors duration-200 group text-gray-400"
         )}
