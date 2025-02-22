52f05bb6dd5b22e7431cd72a61ecda4051cb5805
Fabian Lessmann <lessmannwebdev@gmail.com>
2025-02-22 10:10:29 +0100

    fixed banner height

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -17,7 +17,7 @@ function Banner() {
 
   return (
     <motion.div
-      className="w-full max-h-[calc(100vh/2)]  overflow-hidden bg-red-200 flex items-center justify-center relative"
+      className="w-full max-h-[calc(100vh/2.8)]  overflow-hidden bg-red-200 flex items-center justify-center relative"
       onViewportEnter={() => {
         if (videoRef.current) {
           videoRef.current.muted = true; // Muted für Autoplay aktivieren
