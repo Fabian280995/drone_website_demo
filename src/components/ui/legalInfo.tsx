9c0c725a1fdf38f3127260514e939fcbb84285cf
Fabian Lessmann <lessmannwebdev@gmail.com>
2025-02-22 13:38:54 +0100

    legalinfo textstyling

--- src/components/ui/legalInfo.tsx
+++ src/components/ui/legalInfo.tsx
@@ -8,7 +8,7 @@ const LegalInfo = ({
 }: PropsWithChildren<{ title?: string; className?: string }>) => {
   return (
     <div className={cn("mt-8", className)}>
-      {title && <h2>{title}</h2>}
+      {title && <h2 className="font-semibold">{title}</h2>}
       <div className=" border-l-4 border-primary pl-4 mt-4">{children}</div>
     </div>
   );
